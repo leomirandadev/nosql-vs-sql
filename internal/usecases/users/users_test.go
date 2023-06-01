635d18124a9460334ce9f0d2cb59f399918e070e
Leonardo Antonio Miranda <leomiranda@Leonardos-MacBook-Pro.local>
2023-06-01 14:55:11 -0300

    fix: bench naming

--- internal/usecases/users/users_test.go
+++ internal/usecases/users/users_test.go
@@ -27,37 +27,37 @@ var mariadbConn = configs.NewPostgresConn()
 var repoMariaDB = sql.New(mariadbConn, false)
 var usersUseCaseMariaDB = New(repoMariaDB)
 
-func BenchmarkGetDetailsAllPostgres(b *testing.B) {
+func BenchmarkUsersGetDetailsAllPostgres(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		usersUseCasePostgres.GetDetailsAll(ctx)
 	}
 }
 
-func BenchmarkGetDetailsAllMariaDB(b *testing.B) {
+func BenchmarkUsersGetDetailsAllMariaDB(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		usersUseCaseMariaDB.GetDetailsAll(ctx)
 	}
 }
 
-func BenchmarkGetDetailsAllMongo(b *testing.B) {
+func BenchmarkUsersGetDetailsAllMongo(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		usersUseCaseMongo.GetDetailsAll(ctx)
 	}
 }
 
-func BenchmarkGetDetailsByIDPostgres(b *testing.B) {
+func BenchmarkUsersGetDetailsByIDPostgres(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		usersUseCasePostgres.GetDetailsByID(ctx, "1")
 	}
 }
 
-func BenchmarkGetDetailsByIDMariaDB(b *testing.B) {
+func BenchmarkUsersGetDetailsByIDMariaDB(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		usersUseCaseMariaDB.GetDetailsByID(ctx, "1")
 	}
 }
 
-func BenchmarkGetDetailsByIDMongo(b *testing.B) {
+func BenchmarkUsersGetDetailsByIDMongo(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		usersUseCaseMongo.GetDetailsByID(ctx, "6477c2ffc1adaf0e33063c09")
 	}
